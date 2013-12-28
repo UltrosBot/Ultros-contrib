50d85b5fe9266fbd803839d77bfca5c9b3b9ab48
rakiru <rabbitkillrun@googlemail.com>
2013-12-28 17:28:57 +0000

    DrunkenOctopus v0.1.0 - Initial version

---
+++ DrunkOctopus/README.md
@@ -0,0 +1,12 @@
+DrunkOctopus
+===========
+
+This plugin gives your squidopus shots of vodka and rum.
+
+## Configuration
+
+* `drunkenness` - How drunk the bot should be.
+
+## Attribution
+
+* Sean Gordon
---
+++ DrunkOctopus/plugins/drunkoctopus/__init__.py
@@ -0,0 +1,300 @@
+import random
+import re
+import string
+from system.event_manager import EventManager
+
+__author__ = 'Sean'
+
+import requests
+
+from system.command_manager import CommandManager
+from system.plugin import PluginObject
+from utils.config import YamlConfig
+from utils.data import YamlData
+
+
+class Plugin(PluginObject):
+
+    commands = None
+    config = None
+
+    def setup(self):
+        ### Grab important shit
+        self.commands = CommandManager.instance()
+        self.events = EventManager.instance()
+
+        ### Initial config load
+        try:
+            self.config = YamlConfig("plugins/drunkoctopus.yml")
+        except Exception:
+            self.logger.exception("Error loading configuration!")
+            self.logger.error("Disabling...")
+            self._disable_self()
+            return
+        if not self.config.exists:
+            self.logger.error("Unable to find config/plugins/drunkoctopus.yml")
+            self.logger.error("Disabling...")
+            self._disable_self()
+            return
+
+        ### Create vars and stuff
+        self._drunktalk = DrunkTalk()
+
+        ### Load options from config
+        self._load()
+
+        self.events.add_callback("MessageSent",
+                                 self,
+                                 self.outgoing_message_handler,
+                                 1)
+        self.commands.register_command("drunkenness",
+                                       self.drunkenness_command,
+                                       self,
+                                       "drunkoctopus.drunkenness")
+
+    def reload(self):
+        try:
+            self.config.reload()
+        except Exception:
+            self.logger.exception("Error reloading configuration!")
+            return False
+        self._load()
+        return True
+
+    def _load(self):
+        self._drunktalk.drunkenness = self.config["drunkenness"]
+
+    def drunkenness_command(self, caller, source, args, protocol):
+        if len(args) == 0:
+            caller.respond("Drunkenness level: %s" %
+                           self._drunktalk.drunkenness)
+            return
+        elif len(args) == 1:
+            try:
+                new_drunk = int(args[0])
+                self._drunktalk.drunkenness = new_drunk
+                caller.respond("New drunkenness level: %s" %
+                               self._drunktalk.drunkenness)
+                return
+            except:
+                caller.respond("Invalid drunkenness level (use without "
+                               "arguments for usage)")
+        else:
+            caller.respond("Usage: {CHARS}drunkenness [percent level]")
+
+    def outgoing_message_handler(self, event):
+        """
+        :type event: MessageSent
+        """
+        self.logger.debug("RECEIVED %s EVENT: %s" % (event.type, event.message))
+        event.message = self._drunktalk.drunk_typing(event.message)
+
+
+class DrunkTalk(object):
+
+    # A dict of lowercase to uppercase keys
+    _shifted_chars = {
+        "," : "<",
+        "." : ">",
+        "/" : "?",
+        "\\" : "|",
+        "-" : "_",
+        "=" : "+",
+        "`" : "~",
+        "[" : "{",
+        "]" : "}",
+        ";" : ":",
+        "'" : '"',
+        "#" : "~",
+        "1" : "!",
+        "2" : "@",
+        "3" : "#",
+        "4" : "$",
+        "5" : "%",
+        "6" : "^",
+        "7" : "&",
+        "8" : "*",
+        "9" : "(",
+        "0" : ")"
+    }
+
+    for c in string.ascii_lowercase:
+        _shifted_chars[c] = c.upper()
+
+    _unshifted_chars = {}
+    for k, v in _shifted_chars.iteritems():
+        _unshifted_chars[v] = k
+
+    # Dict of nearby punctuation (same keyboard line, neightbouring keys)
+    _nearby_punct = {
+        "," : ["."],
+        "." : [",", "/"],
+        "/" : ["."],
+        ";" : ["'"],
+        "'" : [";"],
+        "[" : ["]"],
+        "]" : ["[", "\\"]
+    }
+
+    _add = {
+        "~" : ["!"],
+        "!" : ["~", "@"],
+        "@" : ["!", "#"],
+        "#" : ['"', "$"],
+        "$" : ["#", "%"],
+        "%" : ["$", "^"],
+        "^" : ["%", "&"],
+        "&" : ["^", "*"],
+        "*" : ["&", "("],
+        "(" : ["*", ")"],
+        ")" : ["(", "_"],
+        "_" : [")", "+"],
+        "+" : ["="]
+    }
+    for k, v in _nearby_punct.iteritems():
+        _v = [_shifted_chars[c] for c in v]
+        _add[_shifted_chars[k]] = _v
+
+    _nearby_punct.update(_add)
+    del _add
+
+    def __init__(self, drunkenness=100):
+        self.drunkenness = drunkenness
+        # Not making these constants in case I want to make them configurable
+        # _c = chance, _s = spacing (min spacing between changes)
+        self._c_space_mixup = 8
+        self._c_letter_mixup = 10
+        self._c_shift_mixup = 10
+        self._c_punct_remove = 30
+        self._c_punct_double = 5
+        self._c_punct_similar = 8
+        self._s_letter_mixup = 5
+
+    @property
+    def drunkenness(self):
+        return self._drunkenness
+
+    @drunkenness.setter
+    def drunkenness(self, value):
+        self._drunkenness = value
+        self._multiplier = value / 100
+
+    def drunk_typing(self, msg):
+        drunk = self._drunkenness
+        while drunk > 100:
+            msg = self._drunk_typing(msg)
+            drunk -= 100
+        return self._drunk_typing(msg)
+
+    def _drunk_typing(self, msg):
+        msg = self._mixup_spaces(msg)
+        msg = self._mixup_letters(msg)
+        msg = self._mixup_shift(msg)
+        msg = self._remove_punctuation(msg)
+        msg = self._double_punctuation(msg)
+        msg = self._nearby_punctuation(msg)
+        return msg
+
+    def _roll(self, chance):
+        return random.randint(0, 99) < (chance * self._multiplier)
+
+    def _mixup_spaces(self, msg):
+        msgl = list(msg)
+        pos = msg.find(" ")
+        while pos != -1:
+            if random.randint(0, 99) < self._c_space_mixup:
+                direction = 1
+                if pos == 0:
+                    direction = 1
+                elif pos == len(msgl) - 1:
+                    direction = -1
+                else:
+                    direction = random.choice([-1, 1])
+                pos2 = pos + direction
+                msgl[pos], msgl[pos2] = msgl[pos2], msgl[pos]
+            pos = msg.find(" ", pos + 1)
+        return ''.join(msgl)
+
+    def _mixup_letters(self, msg):
+        msgl = list(msg)
+        match = re.search(r"\w\w", msg)
+        start = 0
+        while match != None:
+            pos = match.start() + start
+            if random.randint(0, 99) < self._c_letter_mixup:
+                pos2 = pos + 1
+                msgl[pos], msgl[pos2] = msgl[pos2], msgl[pos]
+                start = pos + 1
+                # re.search doesn't accept a start position, so feed it a
+                # substring
+            start = pos + self._s_letter_mixup
+            msgsub = msg[start:]
+            match = re.search(r"\w\w", msgsub)
+        return ''.join(msgl)
+
+    def _mixup_shift(self, msg):
+        msgl = list(msg)
+        # For this, we need to find a place where a shift key was pressed or
+        # released, and press/release it one character earlier or later
+        shift = msgl[0] in self._unshifted_chars
+        just_altered = False
+        for x in xrange(1, len(msgl)):
+            new_shift = msgl[x] in self._unshifted_chars
+            if just_altered:
+                shift = new_shift
+                just_altered = False
+                continue
+            if new_shift != shift:
+                if random.randint(0, 99) < self._c_shift_mixup:
+                    # Function to check if it's possible to shift-swap a
+                    # position
+                    suitable = lambda p, s: msgl[p] in (
+                        self._unshifted_chars if s else self._shifted_chars)
+
+                    # Change current character (shift late)
+                    pos = x
+                    shifted = new_shift
+                    if random.choice([True, False]) or not suitable(pos,
+                                                                    shifted):
+                        # Actually, change previous character (shift early)
+                        pos = x - 1
+                        shifted = shift
+                        if not suitable(pos, shifted):
+                            shift = new_shift
+                            continue
+
+                    if shifted:
+                        msgl[pos] = self._unshifted_chars[msgl[pos]]
+                    else:
+                        msgl[pos] = self._shifted_chars[msgl[pos]]
+                    just_altered = True
+            shift = new_shift
+        return ''.join(msgl)
+
+    def _remove_punctuation(self, msg):
+        msgl = list(msg)
+        removed = 0
+        for n, c in enumerate(msg):
+            if ((c in string.punctuation and
+                         random.randint(0, 99) < self._c_punct_remove)):
+                del msgl[n - removed]
+                removed += 1
+        return ''.join(msgl)
+
+    def _double_punctuation(self, msg):
+        msgl = list(msg)
+        added = 0
+        for n, c in enumerate(msg):
+            if ((c in string.punctuation and
+                         random.randint(0, 99) < self._c_punct_double)):
+                msgl.insert(n + added, c)
+                added += 1
+        return ''.join(msgl)
+
+    def _nearby_punctuation(self, msg):
+        msgl = list(msg)
+        for n, c in enumerate(msgl):
+            if ((c in self._nearby_punct and
+                         random.randint(0, 99) < self._c_punct_similar)):
+                msgl[n] = random.choice(self._nearby_punct[c])
+        return ''.join(msgl)
