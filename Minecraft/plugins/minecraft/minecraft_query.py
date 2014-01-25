56e8d3049c28474d7abd6af3f169659d1abdd61d
Gareth Coles <gdude2002@gmail.com>
2014-01-25 00:50:12 +0000

    Never mind, I figured it out.

--- Minecraft/plugins/minecraft/minecraft_query.py
+++ Minecraft/plugins/minecraft/minecraft_query.py
@@ -1,186 +1,149 @@
-# coding=utf-8
-__author__ = 'Gareth Coles'
+# flake8: noqa
+# Found: https://github.com/Dinnerbone/mcstatus/blob/master/minecraft_query.py
 
-import json
-import urllib
+import socket
+import struct
 
-from minecraft_query import MinecraftQuery
-from twisted.internet import reactor
-
-from system.command_manager import CommandManager
-from system.decorators import run_async_daemon
-from system.plugin import PluginObject
-from system.protocols.generic.user import User
-from utils.config import YamlConfig
-
-
-class Plugin(PluginObject):
-
-    config = None
-    commands = None
-
-    do_relay = False
-    relay_targets = []
-
-    status_url = "http://status.mojang.com/check"
-    status_refresh_rate = 20
-
-    statuses = {
-        "minecraft.net": "???",
-        "login.minecraft.net": "???",
-        "session.minecraft.net": "???",
-        "account.mojang.com": "???",
-        "auth.mojang.com": "???",
-        "skins.minecraft.net": "???",
-        "authserver.mojang.com": "???",
-        "sessionserver.mojang.com": "???"
-    }
-    status_friendly_names = {
-        "minecraft.net": "Website",
-        "login.minecraft.net": "Login",
-        "session.minecraft.net": "Session",
-        "account.mojang.com": "Account",
-        "auth.mojang.com": "Auth",
-        "skins.minecraft.net": "Skins",
-        "authserver.mojang.com": "Auth server",
-        "sessionserver.mojang.com": "Session server"
-    }
-
-    def setup(self):
-        self.logger.debug("Entered setup method.")
+class MinecraftQuery:
+    MAGIC_PREFIX = '\xFE\xFD'
+    PACKET_TYPE_CHALLENGE = 9
+    PACKET_TYPE_QUERY = 0
+    HUMAN_READABLE_NAMES = dict(
+        game_id     = "Game Name",
+        gametype    = "Game Type",
+        motd        = "Message of the Day",
+        hostname    = "Server Address",
+        hostport    = "Server Port",
+        map         = "Main World Name",
+        maxplayers  = "Maximum Players",
+        numplayers  = "Players Online",
+        players     = "List of Players",
+        plugins     = "List of Plugins",
+        raw_plugins = "Raw Plugin Info",
+        software    = "Server Software",
+        version     = "Game Version",
+    )
+    
+    def __init__(self, host, port, timeout=10, id=0, retries=2):
+        self.addr = (host, port)
+        self.id = id
+        self.id_packed = struct.pack('>l', id)
+        self.challenge_packed = struct.pack('>l', 0)
+        self.retries = 0
+        self.max_retries = retries
+        
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.settimeout(timeout)
+    
+    def send_raw(self, data):
+        self.socket.sendto(self.MAGIC_PREFIX + data, self.addr)
+    
+    def send_packet(self, type, data=''):
+        self.send_raw(struct.pack('>B', type) + self.id_packed + self.challenge_packed + data)
+    
+    def read_packet(self):
+        buff = self.socket.recvfrom(1460)[0]
+        type = struct.unpack('>B', buff[0])[0]
+        id = struct.unpack('>l', buff[1:5])[0]
+        return type, id, buff[5:]
+    
+    def handshake(self, bypass_retries=False):
+        self.send_packet(self.PACKET_TYPE_CHALLENGE)
+        
         try:
-            self.config = YamlConfig("plugins/minecraft.yml")
-        except Exception:
-            self.logger.exception("Error loading configuration!")
-            self._disable_self()
-            return
-        if not self.config.exists:
-            self.logger.error("Unable to find config/plugins/minecraft.yml")
-            self.logger.error("Disabling..")
-            self._disable_self()
-            return
-
-        self.do_relay = self.config["relay_status"]
-        self.relay_targets = self.config["targets"]
-
-        if not self.relay_targets:
-            self.logger.warn("No valid target protocols found. "
-                             "Disabling status relaying.")
-            self.do_relay = False
-
-        self.commands = CommandManager.instance()
-        self.commands.register_command("mcquery", self.query_command, self,
-                                       "minecraft.query")
-
-        if self.do_relay:
-            reactor.callLater(60, self.check_status)
-
-    @run_async_daemon
-    def query_command(self, protocol, caller, source, command, raw_args,
-                      parsed_args):
-        if len(parsed_args) < 1:
-            caller.respond("Usage: {CHARS}mcquery <address> [port]")
-        address = parsed_args[0]
-        port = "25565"
-        if len(parsed_args) > 1:
-            port = parsed_args[1]
-
-        target = source
-        if isinstance(source, User):
-            target = caller
-
-        try:
-            port = int(port)
+            type, id, buff = self.read_packet()
         except:
-            caller.respond("'%s' is not a number.")
-            return
-
+            if not bypass_retries:
+                self.retries += 1
+            
+            if self.retries < self.max_retries:
+                self.handshake(bypass_retries=bypass_retries)
+                return
+            else:
+                raise
+        
+        self.challenge = int(buff[:-1])
+        self.challenge_packed = struct.pack('>l', self.challenge)
+    
+    def get_status(self):
+        if not hasattr(self, 'challenge'):
+            self.handshake()
+        
+        self.send_packet(self.PACKET_TYPE_QUERY)
+        
         try:
-            q = MinecraftQuery(address, int(port))
-            status = q.get_rules()
-        except Exception as e:
-            target.respond("Error retrieving status: %s" % e)
-            return
-
-        done = ""
-        done += "[%s] %s | " % (status["version"], status["motd"])
-        done += "%s/%s " % (status["numplayers"], status["maxplayers"])
-        if "software" in status:
-            done += "| %s " % status["software"]
-        if "plugins" in status:
-            done += "| %s plugins" % len(status["plugins"])
-
-        target.respond(done)
-        if protocol.can_flood and "players" in status \
-           and len(status["players"]):
-            players = ", ".join(status["players"])
-            target.respond("Players: %s" % players)
-
-    @run_async_daemon
-    def check_status(self, firstparse=False):
-        try:
-            r = urllib.urlopen(self.status_url)
-            d = r.read()
-
-            parsed_statuses = {}
-
-            online = []
-            offline = []
-            problems = []
-
-            data = json.loads(d)
-            for server in data:
-                for key, value in server.items():
-                    parsed_statuses[key] = value
-                    if self.statuses[key] != value:
-                        self.logger.debug(u"%s » %s" % (key, value))
-                        if value == "green":
-                            online.append(self.status_friendly_names[key])
-                        elif value == "yellow":
-                            problems.append(self.status_friendly_names[key])
-                        else:
-                            offline.append(self.status_friendly_names[key])
-
-            self.logger.info("%s status changes found." % (len(online)
-                                                           + len(offline)
-                                                           + len(problems)))
-
-            message = "Mojang status report "
-            times = 0
-            for element in online:
-                if times:
-                    message += "| %s » Online " % element
-                else:
-                    message += "[ %s » Online " % element
-                times += 1
-            for element in problems:
-                if times:
-                    message += "| %s » Problems " % element
-                else:
-                    message += "[ %s » Problems " % element
-                times += 1
-            for element in offline:
-                if times:
-                    message += "| %s » Offline " % element
-                else:
-                    message += "[ %s » Offline " % element
-                times += 1
-
-            if times:
-                message += "]"
-                self.relay_message(message, firstparse)
-
-            self.statuses = parsed_statuses
+            type, id, buff = self.read_packet()
         except:
-            self.logger.exception("Error checking Mojang status")
-        finally:
-            reactor.callLater(self.status_refresh_rate, self.check_status)
-
-    def relay_message(self, message, first=False):
-        for target in self.relay_targets:
-            proto = self.factory_manager.get_protocol(target["protocol"])
-            if not proto:
-                self.logger.warn("Protocol not found: %s" % target["protocol"])
-            if first and not target["initial-relay"]:
-                continue
-            proto.send_msg(target["target"], message, target["target-type"])
+            self.handshake()
+            return self.get_status()
+        
+        data = {}
+        
+        data['motd'], data['gametype'], data['map'], data['numplayers'], data['maxplayers'], buff = buff.split('\x00', 5)
+        data['hostport'] = struct.unpack('<h', buff[:2])[0]
+        buff = buff[2:]
+        data['hostname'] = buff[:-1]
+        
+        for key in ('numplayers', 'maxplayers'):
+            try:
+                data[key] = int(data[key])
+            except:
+                pass
+        
+        return data
+    
+    def get_rules(self):
+        if not hasattr(self, 'challenge'):
+            self.handshake()
+        
+        self.send_packet(self.PACKET_TYPE_QUERY, self.id_packed)
+        
+        try:
+            type, id, buff = self.read_packet()
+        except:
+            self.retries += 1
+            if self.retries < self.max_retries:
+                self.handshake(bypass_retries=True)
+                return self.get_rules()
+            else:
+                raise
+        
+        data = {}
+        
+        buff = buff[11:] # splitnum + 2 ints
+        items, players = buff.split('\x00\x00\x01player_\x00\x00') # Shamefully stole from https://github.com/barneygale/MCQuery
+        
+        if items[:8] == 'hostname':
+            items = 'motd' + items[8:]
+        
+        items = items.split('\x00')
+        data = dict(zip(items[::2], items[1::2]))
+        
+        players = players[:-2]
+        
+        if players:
+            data['players'] = players.split('\x00')
+        else:
+            data['players'] = []
+        
+        for key in ('numplayers', 'maxplayers', 'hostport'):
+            try:
+                data[key] = int(data[key])
+            except:
+                pass
+        
+        data['raw_plugins'] = data['plugins']
+        data['software'], data['plugins'] = self.parse_plugins(data['raw_plugins'])
+        
+        return data
+    
+    def parse_plugins(self, raw):
+        parts = raw.split(':', 1)
+        server = parts[0].strip()
+        plugins = []
+        
+        if len(parts) == 2:
+            plugins = parts[1].split(';')
+            plugins = map(lambda s: s.strip(), plugins)
+        
+        return server, plugins
