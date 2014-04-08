90d30c04c7b5a1ebbeb343a555ad5588f6f91e96
Gareth <gdude2002@gmail.com>
2014-04-08 11:43:56 +0100

    Add plugin for testing for Heatbleed vulns

---
+++ Heartbleed/README.md
@@ -0,0 +1,17 @@
+Heartbleed
+===========
+
+This plugin allows you to check for hosts that may be vulnerable to the new
+Heartbleed OpenSSL exploit.
+
+## Commands
+
+* `hb <host> [port]` - Check a host.
+
+## Permissions
+
+* `hb.hb` - Use the `hb` command.
+
+## Attribution
+
+* Gareth Coles
---
+++ Heartbleed/plugins/heartbleed/__init__.py
@@ -0,0 +1,50 @@
+__author__ = 'Gareth Coles'
+
+from . import hb
+
+from system.command_manager import CommandManager
+from system.plugin import PluginObject
+
+
+class Plugin(PluginObject):
+    commands = None
+
+    def setup(self):
+        self.commands = CommandManager()
+
+        self.commands.register_command("hb",
+                                       self.hb_command,
+                                       self,
+                                       "hb.hb")
+
+    def hb_command(self, protocol, caller, source, command, raw_args,
+                            parsed_args):
+        args = raw_args.split()  # Quick fix for new command handler signature
+        if len(args) < 1:
+            caller.respond("Usage: {CHARS}hb <address> [port]")
+            return
+        else:
+            host = args[0]
+            port = 443
+
+            if len(args) > 1:
+                port = args[1]
+                try:
+                    port = int(port)
+                except:
+                    source.respond("Port '%s' is invalid, trying on port "
+                                   "443." % port)
+            try:
+                source("Checking %s:%s" % (host, port))
+                result = hb.try_host(host, port)
+                if result:
+                    source.respond("Host %s is vulnerable!" % host)
+                elif result is None:
+                    source.respond("Host %s returned an error. It's probably "
+                                   "not vulnerable." % host)
+                else:
+                    source.respond("Host %s is not vulnerable." % host)
+            except:
+                self.logger.exception("Error querying host %s" % host)
+                source.respond("Unable to determine whether host %s is "
+                               "vulnerable." % host)
