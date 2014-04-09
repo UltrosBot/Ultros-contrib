63d01aaf7eac1cb55bca50d12475d127f47e5e51
Gareth <gdude2002@gmail.com>
2014-04-09 14:00:05 +0100

    Update Web plugin. Now supports login and sessions. THIS IS AN IMPORTANT UPDATE.

--- Web/plugins/web/__init__.py
+++ Web/plugins/web/__init__.py
@@ -7,7 +7,10 @@ import os
 import sys
 import webassets
 
-from bottle import default_app, request, hook, abort, static_file
+from beaker.middleware import SessionMiddleware
+
+from bottle import default_app, request, hook, abort, static_file, redirect, \
+    run, route
 from bottle import mako_template as template
 
 from twisted.internet.error import ReactorAlreadyRunning
@@ -15,13 +18,17 @@ from twisted.internet.error import ReactorAlreadyRunning
 from .events import ServerStartedEvent, ServerStoppedEvent, ServerStoppingEvent
 from .yielder import Yielder
 
+from system.command_manager import CommandManager
 from system.decorators import run_async_daemon
 from system.event_manager import EventManager
 from system.events.general import ReactorStartedEvent
 from system.plugin import PluginObject
+from system.protocols.generic.user import User
 from system.storage.manager import StorageManager
-from system.storage.formats import YAML
+from system.storage.formats import YAML, JSON
 from utils.packages import packages
+from utils.password import mkpasswd
+from utils.misc import AttrDict
 
 
 class BottlePlugin(PluginObject):
@@ -30,12 +37,16 @@ class BottlePlugin(PluginObject):
     host = "127.0.0.1"
     port = 8080
     output_requests = True
+    session_opts = {}
+    _secret = ""
 
     api_routes_list = []
     navbar_items = {}
     additional_headers = []
 
     config = None
+    commands = None
+    data = None
     env = None
 
     packs = None
@@ -67,7 +78,25 @@ class BottlePlugin(PluginObject):
                 self.port = self.config["port"]
                 self.output_requests = self.config["output_requests"]
 
+        try:
+            self.data = self.storage.get_file(self, "data", JSON,
+                                              "plugins/web/data.json")
+        except Exception:
+            self.logger.exception("Error loading data!")
+            self.logger.error("This data file is required. Shutting down...")
+            self._disable_self()
+            return
+
         base_path = "web/static"
+
+        if "secret" not in self.data:
+            self.logger.warn("Generating secret. DO NOT SHARE IT WITH ANYONE!")
+            self.logger.warn("It's stored in data/plugins/web/data.json - "
+                             "keep this file secure!")
+            with self.data:
+                self.data["secret"] = mkpasswd(60, 20, 20, 20)
+
+        self._secret = self.data["secret"]
 
         self.logger.info("Compiling and minifying Javascript and CSS..")
         self.env = webassets.Environment(base_path, "/static")
@@ -113,10 +142,20 @@ class BottlePlugin(PluginObject):
         else:
             self.logger.info("No CSS files were found. Nothing to do.")
 
-        self.app = default_app()
-        self.events = EventManager()
+        self.session_opts = {
+            "session.cookie_expires": True,
+            "data_dir": "data/plugins/web/beaker/data",
+            "lock_dir": "data/plugins/web/beaker/lock",
+            "type": "file",
+            "key": "Ultros",
+            "secret": self._secret
+        }
 
-        @hook('after_request')
+        self.app = default_app()
+        self.app = SessionMiddleware(self.app, self.session_opts)
+        self.events = EventManager()
+        self.commands = CommandManager()
+
         def log_all():
             ip = request.remote_addr
             method = request.method
@@ -125,6 +164,8 @@ class BottlePlugin(PluginObject):
             level = logging.INFO if self.output_requests else logging.DEBUG
 
             self.logger.log(level, "[%s] %s %s" % (ip, method, fullpath))
+
+        hook("before_request")(log_all)
 
         self.events.add_callback("ReactorStarted", self,
                                  self.start_callback,
@@ -152,6 +193,9 @@ class BottlePlugin(PluginObject):
 
             self.add_route("/", ["GET", "POST"], self.index)
             self.add_route("/index", ["GET", "POST"], self.index)
+            self.add_route("/login", ["GET"], self.login)
+            self.add_route("/login", ["POST"], self.login_post)
+            self.add_route("/logout", ["GET", "POST"], self.logout)
 
             self.add_navbar_entry("Home", "/")
 
@@ -159,8 +203,8 @@ class BottlePlugin(PluginObject):
     def _start_bottle(self):
         try:
             try:
-                self.app.run(host=self.host, port=self.port, server='twisted',
-                             quiet=True)
+                run(app=self.app, host=self.host, port=self.port,
+                    server='twisted', quiet=True)
             except ReactorAlreadyRunning:
                 self.logger.debug("Caught ReactorAlreadyRunning error.")
 
@@ -200,34 +244,88 @@ class BottlePlugin(PluginObject):
         self.logger.debug("Adding header: %s" % header)
         self.additional_headers.append(header)
 
-    def add_route(self, path, *args, **kwargs):
-        if path in self.api_routes_list:
+    def add_route(self, path, methods, *args, **kwargs):
+        if not isinstance(methods, list):
+            methods = [methods]
+        _id = "%s|%s" % (path, ", ".join(methods))
+        if _id in self.api_routes_list:
             return False
-        self.api_routes_list.append(path)
+        self.api_routes_list.append(_id)
 
         self.logger.debug("Adding route: %s" % path)
-        self.app.route(path, *args, **kwargs)
+        route(path, methods, *args, **kwargs)
 
         return True
 
     def get_yielder(self):
         return Yielder()
 
-    def wrap_template(self, content, title, nav="Home"):
+    def get_session(self, r=None):
+        if r is None:
+            r = self.get_objects()
+        return r.session
+
+    def get_objects(self):
+        return AttrDict(
+            request=request,
+            abort=abort,
+            hook=hook,
+            static_file=static_file,
+            template=template,
+            session=request.environ.get("beaker.session")
+        )
+
+    def get_authorization(self, r=None):
+        s = self.get_session(r)
+
+        return AttrDict(
+            authorized=s.get("authorized", False),
+            auth_name=s.get("auth_name", None)
+        )
+
+    def is_authorized(self, r=None):
+        a = self.get_authorization(r)
+        return a["authorized"]
+
+    def check_permission(self, perm, r=None):
+        auth = self.get_authorization(r)
+        caller = User(auth["auth_name"], "web")
+
+        caller.auth_name = auth["auth_name"]
+        caller.authorized = auth["authorized"]
+
+        return self.commands.perm_handler.check(perm, caller, "web", "web")
+
+    def wrap_template(self, content, title, nav="Home", r=None,
+                      tpl="web/templates/generic.html", **kwargs):
+        auth = self.get_authorization(r)
         nav_items = copy.deepcopy(self.navbar_items)
         if nav in nav_items:
             nav_items[nav]["active"] = True
-        return template("web/templates/generic.html",
+        return template(tpl,
                         nav_items=nav_items,
                         headers=self.additional_headers,
                         content=content,
-                        title=title)
+                        title=title,
+                        auth=auth,
+                        **kwargs)
+
+    def require_login(self, r=None):
+        if r is None:
+            r = self.get_objects()
+
+        if not self.is_authorized(r):
+            # Login form
+            return False, redirect("/login", 307)
+        # They're logged in
+        return True, None
 
     # endregion
 
     # region Routes
 
     def index(self):
+        auth = self.get_authorization()
         nav_items = copy.deepcopy(self.navbar_items)
         nav_items["Home"]["active"] = True
         return template("web/templates/index.html",
@@ -235,7 +333,58 @@ class BottlePlugin(PluginObject):
                         headers=self.additional_headers,
                         packages=self.packs.get_installed_packages(),
                         plugins=self.factory_manager.loaded_plugins.values(),
-                        factories=self.factory_manager.factories)
+                        factories=self.factory_manager.factories,
+                        auth=auth)
+
+    def login(self):
+        auth = self.get_authorization()
+        nav_items = copy.deepcopy(self.navbar_items)
+        if not self.is_authorized():
+            # Show login form
+            return template("web/templates/login.html",
+                            nav_items=nav_items,
+                            headers=self.additional_headers,
+                            auth=auth,
+                            failed=False)
+        return redirect("/", 307)
+
+    def login_post(self):
+        auth = self.get_authorization()
+        if not self.is_authorized():
+            s = self.get_session()
+
+            username = request.forms.get("username")
+            password = request.forms.get("password")
+
+            result = False
+
+            for handler in self.commands.auth_handlers:
+                result = handler.check_login(username, password)
+                if result:
+                    break
+
+            if result:
+                s["authorized"] = True
+                s["auth_name"] = username
+                s.save()
+            else:
+                nav_items = copy.deepcopy(self.navbar_items)
+                nav_items["Login"]["active"] = True
+                return template("web/templates/login.html",
+                                nav_items=nav_items,
+                                headers=self.additional_headers,
+                                auth=auth,
+                                failed=True)
+        return redirect("/", 303)
+
+    def logout(self):
+        if self.is_authorized():
+            s = self.get_session()
+            del s["authorized"]
+            del s["auth_name"]
+            s.save()
+            s.delete()
+        return redirect("/", 307)
 
     def static(self, path):
         return static_file(path, root="web/static")
