7cebc79e9cffd25253759049f8f58c8f9cff1cd4
Gareth <gdude2002@gmail.com>
2014-05-01 13:54:48 +0100

    Web: Extend yielder a little.

--- Web/plugins/web/yielder.py
+++ Web/plugins/web/yielder.py
@@ -17,5 +17,15 @@ class Yielder(object):
 
     __next__ = next  # Python 3. Because I can.
 
+    def write(self, data):
+        # This will only work if you're using compatible data-types.
+        self.data += data
+
+    def close(self):
+        if not self.done:
+            self.done = True
+
+        return self.done
+
     def __iter__(self):
         return self
