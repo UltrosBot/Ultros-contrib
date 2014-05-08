4a651e08eb260c4497509b0f7daea2b807ed113b
Gareth <gdude2002@gmail.com>
2014-05-08 14:24:39 +0100

    Anti-Mibbit plugin: For when people using mibpaste and miburl are too stubborn.

---
+++ Anti-Mibbit/README.md
@@ -0,0 +1,13 @@
+Anti-Mibbit
+===========
+
+Some people insist on using terrible IRC clients. This is fine, and generally not your problem.
+
+Mibbit provides an additional two questionable services - a URL shortener and barebones pastebin.
+If you're on a corporate or school network, both of these services are pretty likely to be blocked.
+
+This plugin takes links to MibURL and Mibpaste and re-shortens/re-pastebins their targets on
+respectable services, and also warns people that insist on using such horrible services.
+
+* As a pastebin, hastebin.com seems like a good choice here.
+* URLs will be re-shortened using tinyurl.
---
+++ Anti-Mibbit/plugins/antimibbit/__init__.py
@@ -0,0 +1,53 @@
+__author__ = "Gareth Coles"
+
+import requests
+
+import system.plugin as plugin
+from system.plugin_manager import YamlPluginManagerSingleton
+
+HASTEBIN_URL = "http://hastebin.com/documents"
+
+
+class AntiMibbitPlugin(plugin.PluginObject):
+
+    plugs = None
+
+    @property
+    def urls(self):
+        """
+        :rtype: URLsPlugin
+        """
+        return self.plugs.getPluginByName("URLs").plugin_object
+
+    def setup(self):
+        self.plugs = YamlPluginManagerSingleton()
+
+        self.urls.add_handler("miburl.com", self.miburl_handler)
+        self.urls.add_handler("mibpaste.com", self.mibpaste_handler)
+
+    def hastebin(self, data):
+        key = requests.post(HASTEBIN_URL, data).json()["key"]
+        return "http://hastebin.com/%s" % key
+
+    def miburl_handler(self, url):
+        response = requests.get(url)
+        return "Re-shortened: %s - Don't use Mibbit for URL shortening!" \
+               % self.urls.tinyurl(response.url)
+
+    def mibpaste_handler(self, url):
+        response = requests.get(url)
+        #: :type: str
+        content = response.text
+
+        body_start = content.find("<body>") + 9  # Tabs? Really?
+        last_hr = content.rfind("<hr>") - 1
+
+        to_paste = content[body_start:last_hr]
+        to_paste = to_paste.replace("<br/>", "")
+        to_paste = to_paste.replace("<br />", "")
+        to_paste = to_paste.strip("\n").strip(" ")
+
+        hb_url = self.hastebin(to_paste)
+
+        return "Re-pasted: %s - Don't use Mibbit as a pastebin!" \
+               % hb_url
