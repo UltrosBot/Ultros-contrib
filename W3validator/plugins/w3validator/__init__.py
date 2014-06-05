86a289daea9fceffd52165cd40f9125007b826b1
Zarthus <zarthus@zarth.us>
2014-06-05 13:12:02 +0200

    bugfixes, alterations, and optimisations.
    Fixes a NoneType check error by calling w3_validate_url directly,
    Reduce overhead and redundant code,
    Include the amount of warnings and errors in a valid document if any,
    Correctly capitalise an aborted document for consistency,
    Add colours where it feels needed,
    Serveral grammar fixes.

--- W3validator/plugins/w3validator/__init__.py
+++ W3validator/plugins/w3validator/__init__.py
@@ -49,15 +49,14 @@ class W3validatorPlugin(plugin.PluginObject):
         itter = 0
 
         for website in parsed_args:
-            reactor.callLater(itter, source.respond,
-                              self.w3_validate_url(website))
+            reactor.callLater(itter, self.w3_validate_url, website, source)
 
             itter += 1
-            if itter > 3:
+            if itter > 2:
                 break
 
     @run_async_threadpool
-    def w3_validate_url(self, website):
+    def w3_validate_url(self, website, source):
         """Retrieve and return the validity of a website"""
         val_link = "http://validator.w3.org/check?uri=" + website
 
@@ -65,34 +64,36 @@ class W3validatorPlugin(plugin.PluginObject):
         valid = r.headers["x-w3c-validator-status"].lower()
         col_valid = self.w3_colour_status(valid)
 
-        if valid == "valid":
-            return "[{}] The website {} is {}. ({})".format(col_valid, website,
-                                                       col_valid, val_link)
         if valid == "abort":
             # Possibility that the URL is invalid
-            return ("[{}] The validation for \"{}\" aborted unexpectedly."
-                    " Is this a valid URL?".format(col_valid, website))
+            return source.respond("[{}] The validation for \"{}\" "
+                                  "\x0314Aborted\x0F unexpectedly, "
+                                  "is this a valid URI?"
+                                  .format(col_valid, website))
 
-        errors = r.headers["x-w3c-validator-errors"]
         warnings = r.headers["x-w3c-validator-warnings"]
+        errors = r.headers["x-w3c-validator-errors"]
         recursion = r.headers["x-w3c-validator-recursion"]
 
         errstring = ""
-        if errors:
-            errstring += "Errors: {}, ".format(errors)
-        if warnings:
-            errstring += "Warnings: {}, ".format(warnings)
-        if recursion:
-            errstring += "Recursion: {}, ".format(recursion)
-        if errstring:
-            # Remove the ", " from the string.
-            errstring = errstring[:-2]
-            return ("[{}] The website {} is \x0304{}\x0F. {} ({})").format(
-                     col_valid, website, col_valid, errstring, val_link
-            )
+        if errors != "0":
+            errstring += "Errors: \x0304{}\x0F, ".format(errors)
+        if warnings != "0":
+            errstring += "Warnings: \x0307{}\x0F, ".format(warnings)
+        if recursion != "1":
+		    # Level of recursion is one by default.
+            errstring += "Recursion: \x0314{}\x0F, ".format(recursion)
 
-        return "[{}] The website {} is {}. ({})".format(col_valid, website,
-                                                        col_valid, val_link)
+        # Remove the ", " from the string.
+        errstring = errstring[:-2]
+
+        if errstring:
+            return source.respond("[{}] The website {} is {}. {} ({})"
+                                  .format(col_valid, website, col_valid,
+                                          errstring, val_link))
+
+        return source.respond("[{}] The website {} is {}. ({})"
+                              .format(col_valid, website, col_valid, val_link))
 
     def w3_colour_status(self, status):
         """
@@ -107,6 +108,6 @@ class W3validatorPlugin(plugin.PluginObject):
         elif valid == "invalid":
             col_valid = "\x0304Invalid\x0F"
         else:
-            col_valid = "\x0314{}\x0F".format(validity)
+            col_valid = "\x0314{}\x0F".format(status[0].upper() + status[1:])
 
         return col_valid
