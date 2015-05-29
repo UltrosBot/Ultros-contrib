7ed2ce39da5a42dbf3756f8cf2de494af7c6baf6
Gareth Coles <gdude2002@gmail.com>
2015-05-29 10:52:21 +0100

    [Old-plugins] Resolve addresses in GeoIP, don't double-output if an addr can't be found

--- Old-plugins/plugins/geoip/__init__.py
+++ Old-plugins/plugins/geoip/__init__.py
@@ -2,6 +2,7 @@
 __author__ = "Gareth Coles"
 
 import json
+import socket
 import urllib
 
 import system.plugin as plugin
@@ -25,6 +26,12 @@ class GeoIPPlugin(plugin.PluginObject):
         if len(args) < 1:
             caller.respond("Usage: {CHARS}geoip <address>")
         else:
+            try:
+                args = socket.gethostbyname(args[0])
+            except socket.gaierror:
+                source.respond("%s | Unknown host" % args[0])
+                return
+
             addr = urllib.quote_plus(args[0])
             resp = urllib.urlopen(self.api_url % addr)
             data = resp.read()
@@ -42,6 +49,7 @@ class GeoIPPlugin(plugin.PluginObject):
 
                 if not country and not city and not region and not isp:
                     source.respond("%s | Not found" % args[0])
+                    return
 
                 source.respond("%s | %s, %s, %s (%s)" % (
                     args[0], city or "???", region or "???", country or "???",
