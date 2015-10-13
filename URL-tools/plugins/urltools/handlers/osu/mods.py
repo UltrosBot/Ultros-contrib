84037fb22a633b78b1d5bd41b03161c6d449ede4
Gareth Coles <gdude2002@gmail.com>
2015-10-13 18:12:19 +0100

    Osu mods - Abbreviations and removal of ones that shouldn't be visible

--- URL-tools/plugins/urltools/handlers/osu/mods.py
+++ URL-tools/plugins/urltools/handlers/osu/mods.py
@@ -4,33 +4,33 @@ __author__ = 'Gareth Coles'
 
 mods = {
     "None": 0x0,
-    "No Fail": 0x1,
-    "Easy": 0x2,
-    "No Video": 0x4,
-    "Hidden": 0x8,
-    "Hard Rock": 0x10,
-    "Sudden Death": 0x20,
-    "Double Time": 0x40,
+    "NF": 0x1,
+    "EZ": 0x2,
+    # "No Video": 0x4,  # Not used
+    "HD": 0x8,
+    "HR": 0x10,
+    "SD": 0x20,
+    "DT": 0x40,
     "Relax": 0x80,
-    "Half Time": 0x100,
-    "Nightcore": 0x200,  # Only set with Double Time
-    "Flashlight": 0x400,
-    "Autoplay": 0x800,
+    "HT": 0x100,
+    "NC": 0x200,  # Only set with Double Time
+    "FL": 0x400,
+    "Auto": 0x800,  # osubot
     "Spun Out": 0x1000,
-    "Relax 2": 0x2000,  # ???
-    "Perfect": 0x4000,
+    "Autopilot": 0x2000,
+    "PF": 0x4000,  # Only appears with sudden death
     "4K": 0x8000,
     "5K": 0x10000,
     "6K": 0x20000,
     "7K": 0x40000,
     "8K": 0x80000,
-    "Modded Keys": 0xF8000,  # 4K | 5K | 6K | 7K | 8K
-    "Fade In": 0x100000,
+    # "Modded Keys": 0xF8000,  # 4K | 5K | 6K | 7K | 8K
+    "FI": 0x100000,
     "Random": 0x200000,
-    "Last Mod": 0x300000,
-    "Free Mod Allowed": 0xFF5FF,  # NoFail | Easy | Hidden | HardRock |
-                                  # SuddenDeath | Flashlight | FadeIn |
-                                  # Relax | Relax2 | SpunOut | ModdedKeys
+    # "Last Mod": 0x400000,
+    # "Free Mod Allowed": 0xFF5FF,  # NoFail | Easy | Hidden | HardRock |
+    #  SuddenDeath | Flashlight | FadeIn |
+    #  Relax | Relax2 | SpunOut | ModdedKeys
     "9K": 0x1000000,
     "10K": 0x2000000,
     "1K": 0x4000000,
@@ -45,5 +45,12 @@ def get_mods(integer):
     for k, v in mods.items():
         if integer & v:
             found.append(k)
+
+    # Exceptions and special cases
+    if "NC" and "DT" in found:
+        found.remove("DT")
+
+    if "PF" in found and "SD" in found:
+        found.remove("SD")
 
     return found
