19eadcdb25762f90fbfd292c802371a01f7f8f3a
Gareth Coles <gdude2002@gmail.com>
2015-11-15 08:23:16 +0000

    PEP8

--- URL-tools/plugins/urltools/__init__.py
+++ URL-tools/plugins/urltools/__init__.py
@@ -1,9 +1,6 @@
 # coding=utf-8
-import locale
-
 import system.plugin as plugin
 
-from system.plugins.manager import PluginManager
 from system.storage.formats import YAML
 
 from plugins.urls import Priority
