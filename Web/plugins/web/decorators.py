ef9e40c3327c520b1752fd4c4c99bd7ca5bcc02a
Gareth Coles <gdude2002@gmail.com>
2015-11-22 00:15:32 +0000

    [Web] No splats in lambdas, apparently

--- Web/plugins/web/decorators.py
+++ Web/plugins/web/decorators.py
@@ -18,17 +18,27 @@ def check_api(permission=None, required_key=False):
 
             if username is None and required_key:
                 # Invalid API key
-                return lambda *_, **__: self.finish_json(
-                    invalid_key_error()
-                )
+                return finish_invalid_key(self)
 
             if permission is not None and not self.plugin.check_permission(
                 permission, username
             ):
-                return lambda *_, *__: self.finish_json(
-                    permissions_error(permission)
-                )
+                return finish_permissions_error(self, permission)
 
             return func(self, username, *args, **kwargs)
         return inner
     return wrap
+
+
+def finish_invalid_key(self):
+    def inner():
+        self.finish_json(invalid_key_error())
+
+    return inner
+
+
+def finish_permissions_error(self, permission):
+    def inner():
+        self.finish_json(permissions_error(permission))
+
+    return inner
